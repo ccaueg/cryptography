901c8715500d7468f298d6ae5680e1d67df31293
ccaueg <caue.gprieto@gmail.com>
2026-02-20 16:17:44 -0400

    feat: add TransactionRepository for transaction data access

---
+++ src/main/java/dev/caue/cryptography/repository/TransactionRepository.java
@@ -0,0 +1,9 @@
+package dev.caue.cryptography.repository;
+
+import dev.caue.cryptography.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
