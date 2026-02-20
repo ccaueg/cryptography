01944b6b95f414f0f2468335f5cfb6c6273b2748
ccaueg <caue.gprieto@gmail.com>
2026-02-20 17:03:07 -0400

    feat: add global exception handling for transaction not found errors

---
+++ src/main/java/dev/caue/cryptography/exception/TransactionNotFoundException.java
@@ -0,0 +1,7 @@
+package dev.caue.cryptography.exception;
+
+public class TransactionNotFoundException extends RuntimeException {
+    public TransactionNotFoundException(Long id) {
+        super("Transaction not found with id: " + id);
+    }
+}
