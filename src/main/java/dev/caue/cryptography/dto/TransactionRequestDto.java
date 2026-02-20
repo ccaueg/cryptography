a7e938102937f32d0b8682817a95e82bdbbba90c
ccaueg <caue.gprieto@gmail.com>
2026-02-20 17:00:30 -0400

    feat: update TransactionRequestDto

--- src/main/java/dev/caue/cryptography/dto/TransactionRequestDto.java
+++ src/main/java/dev/caue/cryptography/dto/TransactionRequestDto.java
@@ -1,12 +1,9 @@
 package dev.caue.cryptography.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class TransactionRequestDto {
-    private Long id;
     private String userDocument;
     private String creditCardToken;
     private Long value;
