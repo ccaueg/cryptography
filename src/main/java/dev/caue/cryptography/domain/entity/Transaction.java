d552828c4f15065ba05afea24f8fbef4d5de7659
ccaueg <caue.gprieto@gmail.com>
2026-02-20 15:35:46 -0400

    feat: add Transaction entity

---
+++ src/main/java/dev/caue/cryptography/domain/entity/Transaction.java
@@ -0,0 +1,29 @@
+package dev.caue.cryptography.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "transaction")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_document", nullable = false)
+    private String userDocument;
+
+    @Column(name = "credit_card_token", nullable = false)
+    private String creditCardToken;
+
+    @Column(nullable = false)
+    private Long value;
+}
