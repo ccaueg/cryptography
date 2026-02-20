b90c156f96e492b877ea157ab3cd01881472709d
ccaueg <caue.gprieto@gmail.com>
2026-02-20 17:39:26 -0400

    feat: add CRUD operations to TransactionController

--- src/main/java/dev/caue/cryptography/controller/TransactionController.java
+++ src/main/java/dev/caue/cryptography/controller/TransactionController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/transactions")
 @RequiredArgsConstructor
@@ -19,8 +21,24 @@ public class TransactionController {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.create(dto));
     }
 
+    @GetMapping
+    public ResponseEntity<List<TransactionResponseDto>> findAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<TransactionResponseDto> findById(@PathVariable Long id) {
         return ResponseEntity.ok(service.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TransactionResponseDto> update(@PathVariable Long id, @RequestBody TransactionRequestDto dto) {
+        return ResponseEntity.ok(service.update(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
